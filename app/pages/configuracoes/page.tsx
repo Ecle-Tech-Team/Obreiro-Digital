ad08770446edb829e42fdbca3f3eed40142fa413
Lucas de Angelo <lucasdeangelo343@gmail.com>
2025-06-27 10:53:52 -0300

    Add - Alteração de Senha tela de Configurações Pastor e Obreiro

--- app/pages/configuracoes/page.tsx
+++ app/pages/configuracoes/page.tsx
@@ -91,9 +91,27 @@ export default function configuracoes() {
     });
   };
 
+  const notifySuccessAlterPassword = () => {
+    toast.success('Senha atualizada com sucesso!', {
+      position: 'top-center',
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'colored',
+    });
+  };
+
   const [isEmailModalOpen, setIsEmailModalOpen] = useState(false);
   const [isConfirmEmailModalOpen, setIsConfirmEmailModalOpen] = useState(false);
   const [novoEmail, setNovoEmail] = useState('');
+
+  const [isPasswordModalOpen, setIsPasswordModalOpen] = useState(false);
+  const [isConfirmPasswordModalOpen, setIsConfirmPasswordModalOpen] = useState(false);
+  const [novoPassword, setNovoPassword] = useState('');
+  const [confirmarPassword, setConfirmarPassword] = useState('');
 
   return (
     <main>
@@ -129,7 +147,7 @@ export default function configuracoes() {
               <span className="text-gray-500 text-sm">Novo email</span>
             </div>
 
-            <div className="bg-white py-6 px-8 rounded-lg shadow flex flex-col gap-2 hover:shadow-md cursor-pointer">
+            <div className="bg-white py-6 px-8 rounded-lg shadow flex flex-col gap-2 hover:shadow-md cursor-pointer" onClick={() => setIsPasswordModalOpen(true)}>
               <Image src={keyIcon} width={32} height={32} alt='' className="text-black" />
               <span className="font-bold text-black">Alterar Senha</span>
               <span className="text-gray-500 text-sm">Nova senha</span>
@@ -403,6 +421,98 @@ export default function configuracoes() {
                       await api.put(`/cadastro/${id_user}`, { email: novoEmail });
                       setIsConfirmEmailModalOpen(false);
                       notifySuccessAlterEmail();
+                    } catch (error) {
+                      console.error('Erro ao alterar email:', error);
+                      alert('Erro ao alterar email. Tente novamente.');
+                    }
+                  }}
+                  className="px-5 py-2 bg-azul text-white rounded-lg hover:bg-azul-escuro"
+                >
+                  Sim, Alterar
+                </button>
+              </div>
+            </div>
+          </Modal>
+          
+          <Modal
+            isOpen={isPasswordModalOpen}
+            onRequestClose={() => setIsPasswordModalOpen(false)}
+            contentLabel="Alterar Senha"
+            className="fixed inset-0 flex items-center justify-center"            
+          >
+            <div className="flex flex-col justify-center self-center bg-azul mt-[1vh] rounded-lg shadow-xl">
+              <div className='cursor-pointer flex place-content-end rounded-lg'>
+                <Image onClick={() => setIsPasswordModalOpen(false)} src={close} width={40} height={40} alt='close Icon' className='bg-red-500 hover:bg-red-600 rounded-tr-lg'/>
+              </div>
+              
+              <h2 className="text-xl text1 font-bold text-white mb-4 px-10">Nova Senha</h2>
+              
+              <div className="flex flex-col px-10 gap-4">
+
+                <input
+                  type="password"
+                  placeholder="Digite o nova senha"
+                  className="px-4 py-3 rounded-lg text2 text-black"
+                  value={novoPassword}
+                  onChange={(e) => setNovoPassword(e.target.value)}
+                />
+
+                <input
+                  type="password"
+                  placeholder="Confirme a nova senha"
+                  className="px-4 py-3 rounded-lg text2 text-black"
+                  value={confirmarPassword}
+                  onChange={(e) => setConfirmarPassword(e.target.value)}
+                />
+              </div>
+
+              <div className="flex justify-end space-x-3 py-5 pr-4">                
+                <button
+                  onClick={() => {
+                    if (!novoPassword || !confirmarPassword) {
+                      alert('Preencha os dois campos de senha.');
+                      return;
+                    }
+                    if (novoPassword !== confirmarPassword) {
+                      alert('As senhas não coincidem.');
+                      return;
+                    }
+                    setIsPasswordModalOpen(false);
+                    setIsConfirmPasswordModalOpen(true);
+                  }}
+                  className="px-5 py-2 bg-gray-100 hover:bg-gray-200 text-black rounded-lg hover:bg-azul-escuro"
+                >
+                  Avançar
+                </button>
+              </div>
+            </div>
+          </Modal>
+
+          <Modal
+            isOpen={isConfirmPasswordModalOpen}
+            onRequestClose={() => setIsConfirmPasswordModalOpen(false)}
+            contentLabel="Confirmar Alteração de Senha"
+            className="fixed inset-0 flex items-center bg-black bg-opacity-5 justify-center"
+          >
+            <div className="bg-white rounded-xl p-6 w-full max-w-md">
+              <h2 className="text-xl font-bold text-gray-800 mb-4">Confirmar Alteração</h2>
+              
+              <p className="text-gray-600 mb-6">Tem certeza que deseja alterar sua senha?</p>
+
+              <div className="flex justify-end space-x-3">
+                <button
+                  onClick={() => setIsConfirmPasswordModalOpen(false)}
+                  className="px-5 py-2 rounded-lg text-gray-700 hover:bg-gray-100"
+                >
+                  Cancelar
+                </button>
+                <button
+                  onClick={async () => {
+                    try {
+                      const id_user = sessionStorage.getItem('id_user');
+                      await api.put(`/cadastro/${id_user}`, { senha: novoPassword});
+                      setIsConfirmPasswordModalOpen(false);
+                      notifySuccessAlterPassword();
                     } catch (error) {
                       console.error('Erro ao alterar email:', error);
                       alert('Erro ao alterar email. Tente novamente.');
